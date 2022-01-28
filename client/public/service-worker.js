c1e4d9fb526258d7c4cfd9ee21ecc187466d02f6
victorpawlek <pawlek.v03@htlwienwest.at>
2022-01-28 14:13:06 +0100

    App-Frame

---
+++ client/public/service-worker.js
@@ -0,0 +1,6 @@
+if (workbox) {
+  console.log(`Workbox is loaded`);
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+} else {
+  console.log(`Workbox didn't load`);
+}
---
+++ client/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      name: "Where we watch",
+      short_name: "WWW",
+      theme_color: "#963484",
+      background_color: "#28c2ff",
+      icons: [
+        { src: "./img/icons/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+        { src: "./img/icons/android-chrome-512x512.png", sizes: "192x192", type: "image/png" },
+      ],
+    },
+  },
+};
